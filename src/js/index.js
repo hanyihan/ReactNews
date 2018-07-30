b61b932ec46e964ce27b72523e7787fd7473b6f7
hanyihan <steven.han.yi@foxmail.com>
2018-07-30 17:59:30 +0800

    begin react

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,19 @@
 var React = require('react');
 var ReactDOM =  require('react-dom');
+import BodyIndex from './components/bodyindex.js';
+
+class Index  extends React.Component {
+	render() {
+		return (
+			<div>
+				<BodyIndex />
+			</div>
+		);
+	}
+}
+
+
 ReactDOM.render(
-		<h1>Hello world!</h1>,
+		<Index />,
 		document.getElementById("example")
 	);
