3685a638e641a3206c2470f16441cfe0ef5046ec
hanyihan <steven.han.yi@foxmail.com>
2018-08-06 22:59:44 +0800

    compolished login and logout

---
+++ src/js/components/mobile_index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MobileHeader from './mobile_header';
+import MobileFooter from './monile_footer';
+
+export default class MobileIndex extends React.Component {
+	render() {
+		return (
+			<div>
+				<MobileHeader></MobileHeader>
+				<MobileFooter></MobileFooter>
+			</div>
+		)
+	}
+}
