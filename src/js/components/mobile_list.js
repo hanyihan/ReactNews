4f9317aa4bc93c77d7599780f86749bccf3fd9f3
hanyihan <steven.han.yi@foxmail.com>
2018-08-09 16:27:04 +0800

    start details

--- src/js/components/mobile_list.js
+++ src/js/components/mobile_list.js
@@ -16,7 +16,7 @@ export default class MobileList extends React.Component {
 		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=" + this.props.type + "&count=" + this.props.count, myFetchOptions)
 		.then(response => response.json()).then(json => this.setState({news: json}));
 	}
-	render() {
+	render(){
 		const {news} = this.state;
 		const newsList = news.length ?
 				news.map((newsItem, index) => (
@@ -26,7 +26,7 @@ export default class MobileList extends React.Component {
 								<img src={newsItem.thumbnail_pic_s} alt={newsItem.title} />
 							</div>
 							<div className="m_article_info">
-								<div className="m_article_title">
+				 				<div className="m_article_title">
 									<span>{newsItem.title}</span>
 								</div>
 								<div className="m_article_desc clearfix">
